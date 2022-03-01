2ffc8e797960ca0b187d840789d31899473af2e4
Re1ease <sebastianlarsson18@gmail.com>
2022-03-01 17:48:45 +0100

    Update

--- client/main.lua
+++ client/main.lua
@@ -1,39 +1,28 @@
-local blips = false
-
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(5)
-
-        if not blips then
-            generateBlips()
-            blips = true
-        else
-            Citizen.Wait(10000)
-        end
-    end
+CreateThread(function()
+    generateBlips()
 end)
 
 function generateBlips()
     if PL.ActivateSingle then
-        for k, v in pairs(PL.BlipsSingle) do
-            x = AddBlipForCoord(v.coords.x, v.coords.y, v.coords.z)
-            SetBlipDisplay(x, 4)
-            SetBlipSprite(x, v.sprite)
-            SetBlipColour(x, v.color)
-            SetBlipScale(x, v.scale)
-            SetBlipAsShortRange(x, true)
+        for _, v in pairs(PL.BlipsSingle) do
+            blip = AddBlipForCoord(v.coords.x, v.coords.y, v.coords.z)
+            SetBlipDisplay(blip, 4)
+            SetBlipSprite(blip, v.sprite)
+            SetBlipColour(blip, v.color)
+            SetBlipScale(blip, v.scale)
+            SetBlipAsShortRange(blip, true)
             BeginTextCommandSetBlipName("STRING")
             AddTextComponentString(v.name)
-            EndTextCommandSetBlipName(x)
+            EndTextCommandSetBlipName(blip)
         end
     end
 
     if PL.ActivateRadius then
-        for k, v in pairs(PL.BlipsRadius) do
-            x = AddBlipForRadius(v.coords.x, v.coords.y, v.coords.z, v.radius)
-            SetBlipSprite(x, v.sprite)
-            SetBlipColour(x, v.color)
-            SetBlipAlpha(x, v.transparency)
+        for _, v in pairs(PL.BlipsRadius) do
+            blip = AddBlipForRadius(v.coords.x, v.coords.y, v.coords.z, v.radius)
+            SetBlipSprite(blip, v.sprite)
+            SetBlipColour(blip, v.color)
+            SetBlipAlpha(blip, v.transparency)
         end
     end
 end
