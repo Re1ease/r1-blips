5f64b364a30d71d2ff3cbf2bf9bee2f4e1818d25
Re1ease <86981407+Re1ease@users.noreply.github.com>
2024-02-04 21:25:00 +0100

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,28 +1,42 @@
-CreateThread(function()
-    generateBlips()
-end)
+local function InitializeBlips()
+	for i = 1, #Config.BlipsRadius do
+		local v = Config.BlipsRadius[i]
+		local blip = AddBlipForRadius(v.coords.x, v.coords.y, v.coords.z, v.radius)
+		SetBlipSprite(blip, v.sprite)
+		SetBlipColour(blip, v.color)
+		SetBlipAlpha(blip, v.transparency)
+	end
 
-function generateBlips()
-    if PL.ActivateSingle then
-        for _, v in pairs(PL.BlipsSingle) do
-            blip = AddBlipForCoord(v.coords.x, v.coords.y, v.coords.z)
-            SetBlipDisplay(blip, 4)
-            SetBlipSprite(blip, v.sprite)
-            SetBlipColour(blip, v.color)
-            SetBlipScale(blip, v.scale)
-            SetBlipAsShortRange(blip, true)
-            BeginTextCommandSetBlipName("STRING")
-            AddTextComponentString(v.name)
-            EndTextCommandSetBlipName(blip)
-        end
-    end
-
-    if PL.ActivateRadius then
-        for _, v in pairs(PL.BlipsRadius) do
-            blip = AddBlipForRadius(v.coords.x, v.coords.y, v.coords.z, v.radius)
-            SetBlipSprite(blip, v.sprite)
-            SetBlipColour(blip, v.color)
-            SetBlipAlpha(blip, v.transparency)
-        end
-    end
+	for i = 1, #Config.BlipsSingle do
+		local v = Config.BlipsSingle[i]
+		blip = AddBlipForCoord(v.coords.x, v.coords.y, v.coords.z)
+		SetBlipDisplay(blip, 4)
+		SetBlipSprite(blip, v.sprite)
+		SetBlipColour(blip, v.color)
+		SetBlipScale(blip, v.scale)
+		SetBlipAsShortRange(blip, true)
+		BeginTextCommandSetBlipName("STRING")
+		AddTextComponentString(v.name)
+		EndTextCommandSetBlipName(blip)
+	end
 end
+
+local function NewBlip(data)
+	if not data then return false end
+
+	local blip = AddBlipForCoord(data.coords.x, data.coords.y, data.coords.z)
+	SetBlipDisplay(blip, 4)
+	SetBlipSprite(blip, data.sprite or 16)
+	SetBlipColour(blip, data.color or 1)
+	SetBlipScale(blip, data.scale or 0.7)
+	SetBlipAsShortRange(blip, data.shortRange or true)
+	BeginTextCommandSetBlipName("STRING")
+	AddTextComponentString(data.name or "Blip")
+	EndTextCommandSetBlipName(blip)
+
+	return blip
+end exports("NewBlip", NewBlip)
+
+CreateThread(function()
+    InitializeBlips()
+end)
