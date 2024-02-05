2d0bc77f65bb71ab4b3f197d8f6884b7254c0143
Re1ease <sebastianlarsson18@gmail.com>
2024-02-05 18:30:21 +0100

    fix versioncheck

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version "cerulean"
 game "gta5"
 author "re1ease"
-version "1.0.0"
+version "1.0.1"
 lua54 "yes"
 
 shared_script 'shared/*.lua'
--- server/version.lua
+++ server/version.lua
@@ -6,12 +6,13 @@ CreateThread(function()
 	PerformHttpRequest('https://raw.githubusercontent.com/Re1ease/'..resource_name..'/main/version.json', function(errors, result, header)
 		if not result then print('^1Version check disabled.^0') return end
 		result = json.decode(result)
+
 		if current_version < result.version then
-			print("[^2"..resource_name.."^0] [^4NEW UPDATE^0] ["..current_version.."] -> [^4"..result.version.."^0]")
-			print("[^2"..resource_name.."^0] ^4Info: ^0"..result.note)
-		else
-			print("[^1"..resource_name.."^0] [^1ERROR^0] [^1"..current_version.."^0] -> [^2"..result.version.."^0]")
-			print("[^1"..resource_name.."^0] ^4Info: ^0Resource version is incorrect.")
+			print("[^4NEW UPDATE^0] ["..current_version.."] -> [^4"..result.version.."^0]")
+			print("[^4INFO^0] "..result.note)
+		elseif current_version > result.version then
+			print("[^4ERROR^0] ["..current_version.."] -> [^4"..result.version.."^0]")
+			print("[^4INFO^0] ".."Incorrect Version")
 		end
 	end, 'GET')
 end)
